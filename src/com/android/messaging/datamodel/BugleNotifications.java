2e8b56c50eaf1aa8144bad327d1a32119a008cdb
martincz <martincz@mokeedev.com>
2016-10-12 23:19:12 +0800

    Messaging: code cleanup
    Change-Id: I4818f8ff4fa34fca867cb1da4150d2669fa8a817

--- src/com/android/messaging/datamodel/BugleNotifications.java
+++ src/com/android/messaging/datamodel/BugleNotifications.java
@@ -223,7 +223,7 @@ public class BugleNotifications {
         Intent pendingIntent = new Intent();
         pendingIntent.setClass(context, CaptchasReceiver.class);
         pendingIntent.putExtra("captchas", captchas);
-        pendingIntent.putExtra("conversationId", conversationId);
+        pendingIntent.putExtra(ConversationColumns.SMS_THREAD_ID, conversationId);
         PendingIntent captchasIntent = PendingIntent.getBroadcast(context, 0, pendingIntent,
                 PendingIntent.FLAG_UPDATE_CURRENT);
 
