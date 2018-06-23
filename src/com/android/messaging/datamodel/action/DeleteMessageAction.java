a5b68f84ceea9db2de6c304f235a350ac193fa95
Hamster Tian <haotia@gmail.com>
2018-06-23 18:51:20 +0800

    Messaging: fix force close when deleting captcha messages in notification
    * 4bf01786 and later changes in TelephonyProvider cause it to return
      content://sms/inbox/<id>, an Uri under our request, instead of the
      general content://sms/<id>. This uri will be saved to Messaging app's
      local database, and later read by the deletion task.
      However, TelephonyProvider does not recognize the former when deleting
      messages, and thus an unhandled exception is thrown.
    
    * The app will perform a sync between TelephonyProvider and its local
      database, during which the uri for each message will be regenerated
      and become content://sms/<id> . This ensures successful deletion,
      but no such synchronization could happen before the special notification
      for captcha messages are shown.
    
      In order to delete messages before any sync, we have to correct the
      uri manually. This commit introduced a new function to do this.
    
    Change-Id: I98fd15585f4e1526fcaf35af727fad87b9b7a506

--- src/com/android/messaging/datamodel/action/DeleteMessageAction.java
+++ src/com/android/messaging/datamodel/action/DeleteMessageAction.java
@@ -20,6 +20,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.os.Parcel;
 import android.os.Parcelable;
+import android.provider.Telephony.Sms;
 import android.text.TextUtils;
 
 import com.android.messaging.datamodel.BugleDatabaseOperations;
@@ -46,6 +47,16 @@ public class DeleteMessageAction extends Action implements Parcelable {
     private DeleteMessageAction(final String messageId) {
         super();
         actionParameters.putString(KEY_MESSAGE_ID, messageId);
+    }
+
+    private Uri getTelephonyDeletionUri(MessageData message) {
+        Uri messageUri = message.getSmsMessageUri();
+        if (messageUri.toString().startsWith(Sms.Inbox.CONTENT_URI.toString())) {
+            // TelephonyProvider will not recognize URIs in inbox at deletion,
+            // while it returns such URIs (upon our request) at addition.
+            messageUri = Uri.withAppendedPath(Sms.CONTENT_URI, messageUri.getLastPathSegment());
+        }
+        return messageUri;
     }
 
     // Doing this work in the background so that we're not competing with sync
@@ -79,7 +90,7 @@ public class DeleteMessageAction extends Action implements Parcelable {
                 // We may have changed the conversation list
                 MessagingContentProvider.notifyConversationListChanged();
 
-                final Uri messageUri = message.getSmsMessageUri();
+                final Uri messageUri = getTelephonyDeletionUri(message);
                 if (messageUri != null) {
                     // Delete from telephony DB
                     count = MmsUtils.deleteMessage(messageUri);
